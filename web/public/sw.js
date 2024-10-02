66eec666460c2ef84c85cba9f14c24dc18859506
zhaozzhang <70001176+zhaozzhang@users.noreply.github.com>
2024-10-02 13:44:51 +0900

    build Offline PWA environment

---
+++ web/public/sw.js
@@ -0,0 +1,44 @@
+const STATIC_DATA = [
+  '/',
+];
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('cache_v1').then(function(cache) {
+     return cache.addAll(STATIC_DATA);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
+
+self.addEventListener('push', function (event) {
+    if (event.data) {
+      const data = event.data.json()
+      const options = {
+        body: data.body,
+        icon: data.icon || '/icon.png',
+        badge: '/badge.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: '2',
+        },
+      }
+      event.waitUntil(self.registration.showNotification(data.title, options))
+    }
+  })
+   
+self.addEventListener('notificationclick', function (event) {
+  console.log('Notification click received.')
+  event.notification.close()
+  event.waitUntil(clients.openWindow('<https://your-website.com>'))
+})
